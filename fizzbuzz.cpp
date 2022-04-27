d1fe4dfd622c75b04ad06e0a2891b08e0daafe72
Faelfellah <102027359+Faelfellah@users.noreply.github.com>
2022-04-27 10:07:52 +0200

    Add files via upload

---
+++ fizzbuzz.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void fizzbuzz(int n) {
+	
+	
+
+	
+	
+
+}
+
+
+
+int main(int argc, const char * argv[]) {
+	cout << "fizbuzz(10):" << endl;
+	fizzbuzz(10);
+	cout << "-------------" << endl;
+	cout << "fizbuzz(20):" << endl;
+	fizzbuzz(20);
+}
